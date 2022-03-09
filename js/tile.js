1ff48ffb666d4454a8fcece96245ab106fb994e1
Seongyong Park <EuphoricCatface@gmail.com>
2022-03-09 21:15:07 +0900

    Implement selection construction

--- js/tile.js
+++ js/tile.js
@@ -2,12 +2,11 @@ function Tile(position, value) {
     this.x = position.x;
     this.y = position.y;
     this.value = value;
-    
+
     this.previousPosition = null;
 
     Tile.word_construct = "";
-    Tile.selected_coords = [];
-    Tile.selected_tiles = [];
+    Tile.selected_elements = [];
 }
 
 Tile.prototype.dragstart_handler = function (ev) {
@@ -18,16 +17,13 @@ Tile.prototype.dragstart_handler = function (ev) {
     crt.style.display = "none";
     document.body.appendChild(crt);
     ev.dataTransfer.setDragImage(crt, 0, 0);
-
-    console.log("dragstart");
-    ev.target.classList.add("selected");
 };
 
 Tile.prototype.dragenter_handler = function (ev) {
-    console.log("dragenter");
-    ev.target.classList.add("selected");
+    Tile.tryAddTile(ev.target);
 };
 
+/*
 Tile.prototype.dragleave_handler = function (ev) {
     // NOTE: sometimes, the text on a button fires dragleave_handler instead
     console.log("dragleave");
@@ -38,23 +34,121 @@ Tile.prototype.dragleave_handler = function (ev) {
     }
     target.classList.remove("selected");
 };
+*/
 
 Tile.prototype.dragend_handler = function (ev) {
-    console.log("dragend");
     var target = ev.target;
     if (target.nodeName == "#text") {
         console.log("This is text, replacing with parentElement");
         target = target.parentElement;
     }
-    target.classList.remove("selected");
 };
 
 Tile.prototype.drop_handler = function (ev) {
-    console.log("drop");
-    ev.target.classList.remove("selected");
+    ev.preventDefault();
 };
 
 Tile.prototype.dragover_handler = function (ev) {
-    // What exactly does this do...
     ev.preventDefault();
+};
+/*
+Tile.prototype.isNeighbor = function (that) {
+    console.assert(
+        isInteger(this.x) && isInteger(this.y) &&
+            isInteger(that.x) && isInteger(that.y),
+        "Tile coordinate is not Integer"
+    );
+    if ((Math.abs(this.x - that.x) <= 1) &&
+            (Math.abs(this.y - that.y) <= 1))
+        return true;
+    return false;
+};
+*/
+getElementPosition = function (element) {
+    class_list = Array.from(element.classList);
+    var pos_data = class_list.find(element => element.startsWith("tile-position"));
+    // ex) "tile-position-1-2"
+    var x = parseInt(pos_data.charAt(14));
+    var y = parseInt(pos_data.charAt(16));
+
+    return {
+        x: x,
+        y: y
+    };
+};
+
+isNeighborElement = function (one, another) {
+    var one_position = getElementPosition(one);
+    var another_position = getElementPosition(another);
+
+    console.assert(
+        Number.isInteger(one_position.x) && Number.isInteger(one_position.y) &&
+        Number.isInteger(another_position.x) && Number.isInteger(another_position.y),
+        "Tile coordinate is not Integer"
+    );
+
+    if ((Math.abs(one_position.x - another_position.x) <= 1) &&
+            (Math.abs(one_position.y - another_position.y) <= 1))
+        return true;
+    return false;
+};
+
+Tile.tryAddTile = function (element) {
+    console.assert(
+        Tile.selected_elements.length ==
+            Tile.word_construct.length - [...Tile.word_construct.matchAll("Qu")].length,
+        "selected tiles and word length mismatch"
+    );
+
+    if (element.nodeName == "#text"){
+        // unlikely
+        console.warn("tryAddTile: arg is #text node. Trying parent instead.");
+        element = element.parentElement;
+    }
+
+    do { // do ... while as goto replacement
+        if (Tile.selected_elements.length == 0)
+            // start of selection;
+            // adding unconditionally
+            continue; // goto ADD;
+
+        if (Tile.selected_elements.at(-1) === element)
+            // current tile is same as the last selected tile;
+            // ignoring
+            return;
+
+        if (!isNeighborElement(Tile.selected_elements.at(-1), element))
+            // current tile is somehow too far from the last selected tile;
+            // ignoring
+            return;
+
+        if (Tile.selected_elements.length >= 2 &&
+                Tile.selected_elements.at(-2) === element) {
+            // The user has retreated from the last selection;
+            // pop the last tile
+            Tile.popTile();
+            return;
+        }
+
+    } while(false); // ADD:
+
+    Tile.selected_elements.push(element);
+    Tile.word_construct += element.textContent;
+
+    element.classList.add("selected");
+};
+
+Tile.popTile = function () {
+    console.assert(
+        Tile.selected_elements.length ==
+            Tile.word_construct.length - [...Tile.word_construct.matchAll("Qu")].length,
+        "selected tiles and word length mismatch"
+    );
+
+    element = Tile.selected_elements.pop();
+    Tile.word_construct = Tile.word_construct.slice(0, Tile.word_construct.length-1);
+    if (Tile.word_construct.endsWith("Q"))
+        Tile.word_construct = Tile.word_construct.slice(0, Tile.word_construct.length-1);
+
+    element.classList.remove("selected");
 };
