603a1943abe2e7032d93a07f3d5b04272e1ac14d
Seongyong Park <EuphoricCatface@gmail.com>
2022-03-19 15:18:13 +0900

    Add DRAG_DEBUG flag

--- scripts/tile.js
+++ scripts/tile.js
@@ -3,11 +3,13 @@ class Tile {
     pos;
     value;
     prevPos;
+    static DRAG_DEBUG;
     static word_construct;
     static selected_elements;
     static selected_tiles;
     static events;
     constructor(position, value) {
+        Tile.DRAG_DEBUG = false;
         this.pos = position;
         this.value = value;
         this.prevPos = null;
@@ -20,23 +22,35 @@ class Tile {
         // transparent drag object: https://stackoverflow.com/q/27989602/
         ev.dataTransfer.setData("text", "Tile");
         ev.dataTransfer.setDragImage(new Image(0, 0), 0, 0);
+        if (Tile.DRAG_DEBUG)
+            console.log("dragstart");
         // Workaround: sometimes first tile does not register
         Tile.tryAddTile(ev.target, this);
     }
     dragenter_handler(ev) {
+        if (Tile.DRAG_DEBUG) {
+            console.log("dragenter");
+            console.log(this);
+        }
         var target = ev.target;
-        if (ev.dataTransfer.getData("text") != "Tile")
+        if (ev.dataTransfer.getData("text") != "Tile") {
+            if (Tile.DRAG_DEBUG) console.log(ev.dataTransfer.getData("text"));
             return;
+        }
         if (target.className == "tileScore")
             target = target.parentElement;
         Tile.tryAddTile(target, this);
     }
     dragend_handler(_ev) {
+        if (Tile.DRAG_DEBUG)
+            console.log("dragend");
         // "dragEnd" seem to fire after the "drop"
         // "dragEnd" also happens when "drop" fails
         Tile.selection_clear();
     }
     drop_handler(ev) {
+        if (Tile.DRAG_DEBUG)
+            console.log("drop");
         ev.preventDefault();
         // Workaround: duplicate_check
         if (Tile.selected_tiles.length == 0)
@@ -65,6 +79,7 @@ class Tile {
         return true;
     }
     dragover_handler(ev) {
+        // if (DRAG_DEBUG) console.log("dragover");
         ev.preventDefault();
     }
     static selection_clear() {
@@ -81,6 +96,8 @@ class Tile {
         return false;
     }
     static tryAddTile(element, tile) {
+        if (Tile.DRAG_DEBUG)
+            console.log("tryadd start");
         console.assert(Number.isInteger(tile.pos.x) && Number.isInteger(tile.pos.y), "Tile coordinate is not Integer");
         console.assert(Tile.selected_tiles.length == Tile.selected_elements.length, "selected tiles and elements length mismatch");
         console.assert(Tile.selected_elements.length ==
@@ -91,30 +108,46 @@ class Tile {
             element = element.parentElement;
         }
         do { // do ... while as goto replacement
-            if (Tile.selected_elements.length == 0)
+            if (Tile.selected_elements.length == 0) {
+                if (Tile.DRAG_DEBUG)
+                    console.log("tryadd cond 1");
                 // start of selection;
                 // adding unconditionally
                 continue; // goto ADD;
-            if (Tile.selected_elements.at(-1) === element)
+            }
+            if (Tile.selected_elements.at(-1) === element) {
+                if (Tile.DRAG_DEBUG)
+                    console.log("tryadd cond 2");
                 // current tile is same as the last selected tile;
                 // ignoring
                 return;
-            if (!tile.isNeighbor(Tile.selected_tiles.at(-1)))
+            }
+            if (!tile.isNeighbor(Tile.selected_tiles.at(-1))) {
+                if (Tile.DRAG_DEBUG)
+                    console.log("tryadd cond 3");
                 // current tile is somehow too far from the last selected tile;
                 // ignoring
                 return;
+            }
             if (Tile.selected_elements.length >= 2 &&
                 Tile.selected_elements.at(-2) === element) {
+                if (Tile.DRAG_DEBUG)
+                    console.log("tryadd cond 4");
                 // The user has retreated from the last selection;
                 // pop the last tile
                 Tile.popTile();
                 return;
             }
-            if (Tile.selected_elements.find(e => e === element))
+            if (Tile.selected_elements.find(e => e === element)) {
+                if (Tile.DRAG_DEBUG)
+                    console.log("tryadd cond 5");
                 // element is already in the list;
                 // ignoring
                 return;
+            }
         } while (false); // ADD:
+        if (Tile.DRAG_DEBUG)
+            console.log("tryadd cond 6");
         Tile.selected_elements.push(element);
         Tile.selected_tiles.push(tile);
         Tile.word_construct += tile.value;
--- scripts/tile.ts
+++ scripts/tile.ts
@@ -7,11 +7,14 @@ class Tile {
     pos: CoordType;
     value: string;
     prevPos: CoordType;
+    static DRAG_DEBUG: Boolean;
     static word_construct: string;
     static selected_elements: HTMLElement[];
     static selected_tiles: Tile[];
     static events: Map<string, CallableFunction[]>;
     constructor(position: CoordType, value: string) {
+        Tile.DRAG_DEBUG = false;
+
         this.pos = position;
         this.value = value;
 
@@ -27,25 +30,34 @@ class Tile {
         // transparent drag object: https://stackoverflow.com/q/27989602/
         ev.dataTransfer.setData("text", "Tile");
         ev.dataTransfer.setDragImage(new Image(0, 0), 0, 0);
+        if (Tile.DRAG_DEBUG) console.log("dragstart");
 
         // Workaround: sometimes first tile does not register
         Tile.tryAddTile((ev.target as HTMLElement), this);
     }
     dragenter_handler(ev: DragEvent) {
+        if (Tile.DRAG_DEBUG) {
+            console.log("dragenter");
+            console.log(this);
+        }
         var target = (ev.target as HTMLElement);
-        if (ev.dataTransfer.getData("text") != "Tile")
+        if (ev.dataTransfer.getData("text") != "Tile") {
+            if (Tile.DRAG_DEBUG) console.log(ev.dataTransfer.getData("text"));
             return;
+        }
         if (target.className == "tileScore")
             target = target.parentElement;
 
         Tile.tryAddTile(target, this);
     }
     dragend_handler(_ev: DragEvent) {
+        if (Tile.DRAG_DEBUG) console.log("dragend");
         // "dragEnd" seem to fire after the "drop"
         // "dragEnd" also happens when "drop" fails
         Tile.selection_clear();
     }
     drop_handler(ev: DragEvent) {
+        if (Tile.DRAG_DEBUG) console.log("drop");
         ev.preventDefault();
 
         // Workaround: duplicate_check
@@ -78,6 +90,7 @@ class Tile {
         return true;
     }
     dragover_handler(ev: DragEvent) {
+        // if (DRAG_DEBUG) console.log("dragover");
         ev.preventDefault();
     }
     static selection_clear() {
@@ -96,6 +109,7 @@ class Tile {
         return false;
     }
     static tryAddTile(element: HTMLElement, tile: Tile) {
+        if (Tile.DRAG_DEBUG) console.log("tryadd start");
         console.assert(
             Number.isInteger(tile.pos.x) && Number.isInteger(tile.pos.y),
             "Tile coordinate is not Integer"
@@ -117,34 +131,42 @@ class Tile {
         }
 
         do { // do ... while as goto replacement
-            if (Tile.selected_elements.length == 0)
+            if (Tile.selected_elements.length == 0) {
+                if (Tile.DRAG_DEBUG) console.log("tryadd cond 1");
                 // start of selection;
                 // adding unconditionally
                 continue; // goto ADD;
-
-            if (Tile.selected_elements.at(-1) === element)
+            }
+            if (Tile.selected_elements.at(-1) === element) {
+                if (Tile.DRAG_DEBUG) console.log("tryadd cond 2");
                 // current tile is same as the last selected tile;
                 // ignoring
                 return;
+            }
 
-            if (!tile.isNeighbor(Tile.selected_tiles.at(-1)))
+            if (!tile.isNeighbor(Tile.selected_tiles.at(-1))){
+                if (Tile.DRAG_DEBUG) console.log("tryadd cond 3");
                 // current tile is somehow too far from the last selected tile;
                 // ignoring
                 return;
-
+            }
             if (Tile.selected_elements.length >= 2 &&
-                Tile.selected_elements.at(-2) === element) {
+                    Tile.selected_elements.at(-2) === element) {
+                if (Tile.DRAG_DEBUG) console.log("tryadd cond 4");
                 // The user has retreated from the last selection;
                 // pop the last tile
                 Tile.popTile();
                 return;
             }
 
-            if (Tile.selected_elements.find(e => e === element))
+            if (Tile.selected_elements.find(e => e === element)){
+                if (Tile.DRAG_DEBUG) console.log("tryadd cond 5");
                 // element is already in the list;
                 // ignoring
                 return;
+            }
         } while (false); // ADD:
+        if (Tile.DRAG_DEBUG) console.log("tryadd cond 6");
 
         Tile.selected_elements.push(element);
         Tile.selected_tiles.push(tile);
